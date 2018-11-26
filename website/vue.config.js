33f0df90e1040456945e384328d0b99828029fa8
Jake Nichols <jnichols4nrf@gmail.com>
2018-11-26 12:55:15 +0000

    Master commit

---
+++ website/vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  css: {
+    sourceMap: true,
+    modules: true
+  },
+
+  baseUrl: undefined,
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined
+}
