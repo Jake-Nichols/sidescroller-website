fb5c91822cc787d30d4a87c910b08ff10f979183
Jake Nichols <jnichols4nrf@gmail.com>
2018-11-28 09:02:19 +0000

    Added Vue iframe and Vue resource packages

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
 import WebFont from "webfontloader";
+import VueFriendlyIframe from "vue-friendly-iframe";
 import router from "./router";
 import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
+
+Vue.component("vue-friendly-iframe", VueFriendlyIframe);
 
 // use async google fonts
 WebFont.load({
