4dbedbbb439269787413d5d9b4ca53238e95f3c2
Jake Nichols <jnichols4nrf@gmail.com>
2018-11-29 12:51:18 +0000

    Added Netlify form

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -2,5 +2,17 @@
   <div class="game">
     <h1>Play the game below</h1>
     <iframe src="https://google.co.uk"></iframe>
+    <h2>Bug/Feedback Form</h2>
+    <form name="contact" netlify>
+        <p>
+            <label>Name <input type="text" name="name" /></label>
+        </p>
+        <p>
+            <label>Email <input type="email" name="email" /></label>
+        </p>
+        <p>
+            <button type="submit">Send</button>
+        </p>
+    </form>
   </div>
 </template>
