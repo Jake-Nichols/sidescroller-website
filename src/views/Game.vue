14d9198e5a4bf53a38d1b0d06cf9fbb69e1c0d78
Jake Nichols <jnichols4nrf@gmail.com>
2018-11-29 12:55:58 +0000

    Form markup

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -3,16 +3,15 @@
     <h1>Play the game below</h1>
     <iframe src="https://google.co.uk"></iframe>
     <h2>Bug/Feedback Form</h2>
-    <form name="contact" netlify>
+    <form name="bug/feedback" action="/game" netlify>
         <p>
             <label>Name <input type="text" name="name" /></label>
         </p>
         <p>
             <label>Email <input type="email" name="email" /></label>
         </p>
-        <p>
-            <button type="submit">Send</button>
-        </p>
+        <div data-netlify-recaptcha></div>
+        <button type="submit">Send</button>
     </form>
   </div>
 </template>
